7b189d6d80e8f0fd78ace5b3015deef2e220623a
Eran Rom <eran@itsonlyme.name>
2016-08-02 18:50:00 +0300

    New storlet for testing pushdown
    PartitionsIdentityStorlet is a new storlet that has built in support
    for cross record ranges: Spark tasks can break an object in the middle
    of a record. This storlet mimics the mepreduce solution for that
    allowing to bypass the mapreduce layer altogether when doing a pushdown
    from Spark.
    
    Bonus:
    Two minor fixes in the install scripts
    
    Change-Id: I8ac3b0512f7d5a4318e23e459e560c4b8f6835c2

---
+++ StorletSamples/PartitionsIdentityStorlet/src/org/openstack/storlet/PartitionsIdentityStorlet.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright (c) 2015, 2016 OpenStack Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openstack.storlet;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.OutputStream;
+
+import com.ibm.storlet.common.IStorlet;
+import com.ibm.storlet.common.StorletException;
+import com.ibm.storlet.common.StorletInputStream;
+import com.ibm.storlet.common.StorletLogger;
+import com.ibm.storlet.common.StorletObjectOutputStream;
+import com.ibm.storlet.common.StorletContainerHandle;
+import com.ibm.storlet.common.StorletOutputStream;
+import com.ibm.storlet.common.StorletUtils;
+
+/***
+ * An identity storlet for record based text files.
+ * Records are separated with a newline delimiter.
+ * The storlet is used for testing range requests
+ * where ranges can cross records boundaries.
+ * In order not to lose any record split between ranges
+ * we do the following:
+ * Given a range [start, end] the actual range being sent to
+ * the storlet is going to be [start, end + max_record_line]
+ * where the max record line is the maximum length of a record
+ * Note that this range is calculated outside of the storlet
+ * however, the storlet needs to be aware that the range it got
+ * is of the form [start,end + max_record_line]
+ * In addition pass the storlet a boolean 'firstPartition' telling
+ * whether this is the first range of the file or not
+ * With this the storlet proceeds as follows:
+ * (1) If firstPartition is True, the storlet begins at 'start' and
+ *     processes records until it gets to 'end'. Once getting to
+ *     the 'end' position the storlet proceeds until it reaches a
+ *     record termination. This means that if the end position was
+ *     at the middle of a record, the whole record is processed. If,
+ *     however, the end position was exactly at the end of a record,
+ *     the storlet processed on more complete record
+ * (2) If firstPartition is False, the storlet skips the data until
+ *     it reaches a record termination, and starts processing as
+ *     described in (1)
+ */
+public class PartitionsIdentityStorlet implements IStorlet {
+	long m_start, m_end, m_length;
+	int m_max_record_line;
+	boolean m_firstPartition;
+	BufferedReader m_br = null;
+
+        long m_total_lines_emitted = 0;
+
+	StorletLogger m_log;
+
+	private void safeClose(OutputStream os, InputStream is) {
+		try {
+			if (m_br != null) m_br.close();
+		} catch (IOException e) {
+		}
+		try {
+			if (os != null) os.close();
+		} catch (IOException e) {
+		}
+		try {
+			if (is != null) os.close();
+		} catch (IOException e) {
+		}
+	}
+
+	private void parseInputParameters(Map<String, String> parameters) throws Exception {
+		if (parameters.get("start") != null) {
+			m_start = Long.parseLong(parameters.get("start"));
+		} else {
+			m_log.emitLog("Missing mandatory start parameter");
+			throw new Exception("Missing mandatory start parameter");
+		}
+		if (parameters.get("end") != null) {
+			m_end = Long.parseLong(parameters.get("end"));
+		} else {
+			m_log.emitLog("Missing mandatory end parameter");
+			throw new Exception("Missing mandatory end parameter");
+		}
+		if (parameters.get("max_record_line") != null) {
+			m_max_record_line = Integer.parseInt(parameters.get("max_record_line"));
+		} else {
+			m_log.emitLog("Missing mandatory max_record_line parameter");
+			throw new Exception("Missing mandatory max_record_line parameter");
+		}
+		if (parameters.get("first_partition") != null) {
+			m_firstPartition = Boolean.parseBoolean(parameters.get("first_partition"));
+		} else {
+			m_log.emitLog("Missing mandatory first_partition parameter");
+			throw new Exception("Missing mandatory first_partition parameter");
+		}
+		m_length = m_end - m_start;
+	}
+
+	private int consumeFirstLine(OutputStream os) throws IOException {
+		String line;
+		line = m_br.readLine();
+		if (line == null) {
+			m_log.emitLog("m_br fully consumed on first line");
+			throw new IOException("m_br fully consumed on first line");
+		}
+		if (m_firstPartition == true) {
+                        m_total_lines_emitted += 1;
+			//m_log.emitLog("This is the first partition, writing first line");
+			//m_log.emitLog("wrote: " + new String(line.getBytes(),"UTF-8") + "\n");
+			os.write(line.getBytes());
+                	os.write('\n');
+		} else {
+			//m_log.emitLog("This is NOT the first partition, skipping first line");
+		}
+
+		return line.length() + 1;
+	}
+
+	@Override
+	public void invoke(ArrayList<StorletInputStream> inputStreams,
+			ArrayList<StorletOutputStream> outputStreams,
+			Map<String, String> parameters, StorletLogger log)
+			throws StorletException {
+		m_log = log;
+		m_log.emitLog("PartitionsIdentityStorlet Invoked");
+
+                StorletObjectOutputStream sos = null;
+                OutputStream os = null;
+                InputStream is = null;
+                try {
+		    sos = (StorletObjectOutputStream)outputStreams.get(0);
+		    sos.setMetadata(new HashMap<String, String>());
+		    os = sos.getStream();
+		    is = inputStreams.get(0).getStream();
+		} catch (Exception ex) {
+		        m_log.emitLog("Failed to get streams from Storlet invoke inputs");
+			safeClose(os, is);
+                        return;
+		}
+
+		/*
+		 * Get mandatory parameters
+		 */
+		try {
+			parseInputParameters(parameters);
+		} catch (Exception ex) {
+		        m_log.emitLog("Failed to initialize input stream");
+			safeClose(os, is);
+                        return;
+		}
+
+		String line;
+		int lineLength = 0;
+		try {
+			m_br = new BufferedReader(new InputStreamReader(is));
+		} catch (Exception ex) {
+		        m_log.emitLog("Failed to initialize input stream");
+                        safeClose(os, is);
+                        return;
+                }
+
+                try {
+			lineLength = consumeFirstLine(os);
+		} catch (Exception ex) {
+		        m_log.emitLog("Failed to consume first line");
+                        safeClose(os, is);
+                        return;
+                }
+
+		m_length -= lineLength;
+		try {
+			// We allow m_length to get to -1 so as to read an extra record
+			// if m_end points exactly to an end of a record.
+			while ( ((line = m_br.readLine()) != null) && (m_length >= -1) ) {
+                                m_total_lines_emitted += 1;
+				os.write(line.getBytes());
+				os.write('\n');
+				//m_log.emitLog("m_length is " + m_length);
+				//m_log.emitLog("wrote: " + new String(line.getBytes(),"UTF-8") + "\n");
+				m_length -= (line.length() + 1);
+			}
+                        if (m_length > 0)
+			    m_log.emitLog("Got a null line while not consuming all range");
+
+		} catch (Exception ex) {
+			m_log.emitLog("Exception while consuming range " + Arrays.toString(ex.getStackTrace()) );
+		} finally {
+                        safeClose(os, is);
+		}
+                m_log.emitLog("Total lines emitted: " + m_total_lines_emitted);
+	}
+}
---
+++ tests/functional/test_partitions_identity_storlet.py
@@ -0,0 +1,151 @@
+# Copyright (c) 2010-2016 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from swiftclient import client as c
+from __init__ import StorletFunctionalTest
+
+# Below is the records.txt file we are testing with
+# position line content
+# 0        header line
+# 12       random text 1
+# 26       random text 2
+# 40       random text 3
+# 54       random text 4
+# 68       random text 5
+# 82       random text 6
+# 96       random text 7
+# 110      random text 8
+# 124      random text 9
+# 138      random text 10
+# 153      random text 100
+# 169      random text 1000
+# 186      random text 10000
+# 204      ....
+records_txt = \
+    "header line\n\
+random text 1\n\
+random text 2\n\
+random text 3\n\
+random text 4\n\
+random text 5\n\
+random text 6\n\
+random text 7\n\
+random text 8\n\
+random text 9\n\
+random text 10\n\
+random text 100\n\
+random text 1000\n\
+random text 10000\n\
+random text 1\n"
+
+
+class TestPartitionsIdentityStorlet(StorletFunctionalTest):
+    def setUp(self):
+        self.storlet_dir = 'PartitionsIdentityStorlet'
+        self.storlet_name = 'partitionsidentitystorlet-1.0.jar'
+        self.storlet_main = 'org.openstack.storlet.PartitionsIdentityStorlet'
+        self.storlet_log = 'partitionsidentitystorlet-1.0.log'
+        self.storlet_file = 'records.txt'
+        self.container = 'myobjects'
+        self.dep_names = []
+        self.headers = {}
+        self.additional_headers = {}
+        super(TestPartitionsIdentityStorlet, self).setUp()
+
+    def invoke_storlet(self, start, end, first_partition, max_record_line):
+        headers = {'X-Run-Storlet': self.storlet_name}
+        headers.update(self.additional_headers)
+        headers['X-Storlet-Range'] = 'bytes=%d-%d' % (start,
+                                                      end + max_record_line)
+        headers['X-Storlet-Parameter-1'] = '%s:%s' % ('start', start)
+        headers['X-Storlet-Parameter-2'] = '%s:%s' % ('end', end)
+        headers['X-Storlet-Parameter-3'] = '%s:%s' % ('max_record_line',
+                                                      max_record_line)
+        headers['X-Storlet-Parameter-4'] = '%s:%s' % ('first_partition',
+                                                      first_partition)
+        _, content = c.get_object(
+            self.url, self.token, self.container, self.storlet_file,
+            response_dict=dict(), headers=headers)
+        return content
+
+    # The input file in our case has the following characteristics:
+    # It has a header line and actual data starts at byte 13
+    # Since the first byte is at location 0, the 13th byte is at location 12
+    # The max line is 18 chars (including '\n')
+    # Here are the tests we do:
+    # 1. First range tests:
+    #   - range Starts with 13
+    #   - firstLine param is set to True
+    #   - use various max_line_len
+    #   - use various end bytes
+    # 2. Second range tests
+    #   - range can start at any location
+    #   - firstLine param is set to False
+    #   - use various start, end bytes
+
+    def _test_first_range(self, max_record_line):
+        # According to the file's content end=82 should give us
+        # lines 1-5, plus line 6 as the extra line
+        content = self.invoke_storlet(12, 82, 'true', max_record_line)
+        # byte 95 is the newline of line 6
+        self.assertEqual(content, records_txt[12:95] + '\n')
+        # end=83,85,...,94 gets us deeper into the same line,
+        # output should be the same:
+        # lines 1-5, plus line 6 as the extra line
+        for i in xrange(12):
+            content = self.invoke_storlet(12, i + 83, 'true', max_record_line)
+            self.assertEqual(content, records_txt[12:95] + '\n')
+        # Now that we move one extra character we get a different answer
+        content = self.invoke_storlet(12, 95, 'true', max_record_line)
+        self.assertNotEqual(content, records_txt[12:95] + '\n')
+
+    def _test_second_range(self, max_record_line):
+        # According to the file's content start=82 should give us
+        # line 7 as the first line (dropping line 6)
+        # end=185 should give us line 13 as well as an extra line
+        # and it should skip line 6 being the first line
+        content = self.invoke_storlet(82, 185, 'false', max_record_line)
+        self.assertEqual(content, records_txt[96:203] + '\n')
+        # starting from 83 up to 95 should give the exact result
+        for i in xrange(12):
+            content = self.invoke_storlet(i + 83, 185, 'false',
+                                          max_record_line)
+            self.assertEqual(content, records_txt[96:203] + '\n')
+        # ending at any point up to 202 should give the exact result
+        for i in xrange(16):
+            content = self.invoke_storlet(82, 186 + i, 'false',
+                                          max_record_line)
+            self.assertEqual(content, records_txt[96:203] + '\n')
+        # now for the combinations of the two:
+        for i in xrange(12):
+            for j in xrange(16):
+                content = self.invoke_storlet(83 + i, 186 + j, 'false',
+                                              max_record_line)
+                self.assertEqual(content, records_txt[96:203] + '\n')
+
+    def test_second_range(self):
+        self._test_second_range(80)
+
+    def test_first_range(self):
+        for max_record_line in xrange(5):
+            # The maximium line length in this section is 14
+            # includuing the new line. and so any max length >=14
+            # should be good.
+            self._test_first_range(80)
+
+
+class TestPartitionsIdentityStorletOnProxy(TestPartitionsIdentityStorlet):
+    def setUp(self):
+        super(TestPartitionsIdentityStorletOnProxy, self).setUp()
+        self.additional_headers = {'X-Storlet-Run-On-Proxy': ''}
