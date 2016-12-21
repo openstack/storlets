4c7b5d3f68a0c3b697a75eb4717db1acb7cb4987
Akihito Takai <takaiak@nttdata.co.jp>
2016-12-21 16:09:48 +0900

    Add functional tests for python storlet execution with parameters
    This patch adds some funcitonal tests to test capability of storlet
    engine to run python written applications, especially to test that
    it can handle given parameters in request queries or headers.
    
    It introduces the same test cases as the following ones for java
    written storlets, so that we can have the same test coverage for
    python written storlets.
    <tests/functional/java/test_identity_storlet.py>
    
    Change-Id: I70d36767be7df7d3be0f5b4339939a1d524c3f11

---
+++ StorletSamples/python/storlet_samples/exec_query_header/exec_query_header.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2010-2016 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class ExecQueryHeaderStorlet(object):
+    def __init__(self, logger):
+        self.logger = logger
+
+    def __call__(self, in_files, out_files, params):
+        """
+        The function called for storlet invocation
+
+        :param in_files: a list of StorletInputFile
+        :param out_files: a list of StorletOutputFile
+        :param params: a dict of request parameters
+        """
+
+        metadata = in_files[0].get_metadata()
+        for key, value in params.items():
+            metadata[key] = value
+        out_files[0].set_metadata(metadata)
+
+        self.logger.debug('Start to return object data')
+        while True:
+            buf = in_files[0].read(16)
+            if not buf:
+                break
+            self.logger.debug('Recieved %d bytes' % len(buf))
+            out_files[0].write(buf)
+        self.logger.debug('Complete')
+        in_files[0].close()
+        out_files[0].close()
---
+++ tests/functional/python/test_execqueryheader_storlet.py
@@ -0,0 +1,115 @@
+# Copyright (c) 2010-2016 OpenStack Foundation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+import string
+from swiftclient import client
+from tests.functional.python import StorletPythonFunctionalTest
+
+
+class TestExecQueryHeaderStorlet(StorletPythonFunctionalTest):
+    def setUp(self):
+        self.additional_headers = {}
+        super(TestExecQueryHeaderStorlet, self).setUp(
+            storlet_dir='exec_query_header',
+            storlet_name='exec_query_header.py',
+            storlet_main='exec_query_header.ExecQueryHeaderStorlet',
+            container='myobjects',
+            storlet_file='source.txt',
+            headers={})
+
+    def invoke_storlet(self, op, params=None, header_parameters=False):
+        headers = {'X-Run-Storlet': self.storlet_name}
+        headers.update(self.additional_headers)
+        if params is not None:
+            if header_parameters:
+                querystring = None
+                count = 1
+                for key in params:
+                    headers['X-Storlet-Parameter-' + str(count)] = \
+                        key + ':' + params[key]
+                    count = count + 1
+            else:
+                querystring = '&'.join(['%s=%s' % (k, v)
+                                       for k, v in params.items()])
+        else:
+            querystring = None
+
+        if op == 'GET':
+            # Get original object
+            original_h, original_c = client.get_object(self.url, self.token,
+                                                       'myobjects',
+                                                       self.storlet_file,
+                                                       response_dict=dict())
+            file_length = int(original_h['content-length'])
+            processed_h, returned_c = client.get_object(
+                self.url, self.token,
+                self.container, self.storlet_file,
+                query_string=querystring, response_dict=dict(),
+                headers=headers, resp_chunk_size=file_length)
+            processed_c = ''
+            for chunk in returned_c:
+                if chunk:
+                    processed_c += chunk
+
+            for param_key, param_value in params.items():
+                mdv = processed_h['X-Object-Meta-%s'.lower() % param_key]
+                self.assertEqual(mdv, param_value)
+
+        if op == 'PUT':
+            # PUT a random file
+            response = dict()
+            uploaded_c = ''.join(random.choice(string.ascii_uppercase +
+                                 string.digits) for _ in range(1024))
+            content_length = None
+            client.put_object(self.url, self.token, self.container,
+                              'random_source',
+                              uploaded_c, content_length, None, None,
+                              "application/octet-stream", headers, None, None,
+                              querystring, response)
+            resp_headers, saved_c = client.get_object(self.url, self.token,
+                                                      'myobjects',
+                                                      'random_source',
+                                                      response_dict=dict())
+
+            for param_key, param_value in params.items():
+                mdv = resp_headers['X-Object-Meta-%s'.lower() % param_key]
+                self.assertEqual(mdv, param_value)
+
+    def test_put_execute_with_header(self):
+        self.invoke_storlet('PUT', {'key1': 'value1'},
+                            header_parameters=True)
+        self.invoke_storlet('PUT', {'key1': 'value1', 'key2': 'value2'},
+                            header_parameters=True)
+
+    def test_put_execute_with_query(self):
+        self.invoke_storlet('PUT', {'key1': 'value1'})
+        self.invoke_storlet('PUT', {'key1': 'value1', 'key2': 'value2'})
+
+    def test_get_execute_with_header(self):
+        self.invoke_storlet('GET', {'key1': 'value1'},
+                            header_parameters=True)
+        self.invoke_storlet('GET', {'key1': 'value1', 'key2': 'value2'},
+                            header_parameters=True)
+
+    def test_get_execute_with_query(self):
+        self.invoke_storlet('GET', {'key1': 'value1'})
+        self.invoke_storlet('GET', {'key1': 'value1', 'key2': 'value2'})
+
+
+class TestExecQueryHeaderStorletOnProxy(TestExecQueryHeaderStorlet):
+    def setUp(self):
+        super(TestExecQueryHeaderStorletOnProxy, self).setUp()
+        self.additional_headers = {'X-Storlet-Run-On-Proxy': ''}
