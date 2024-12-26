661bb13c53d739559221d44854a7695b3cf4b383
Takashi Kajinami <kajinamit@oss.nttdata.com>
2024-12-26 21:43:14 +0900

    Use shell script to capture command outputs
    ... so that we can capture additional information such as containers
    or container images.
    
    Change-Id: I04d24314287e5524bf318e491a065efb2e317bab

---
+++ tools/copy_logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo 'Copy logs into workspace'
+
+LOG_DIR=$WORKSPACE/logs
+
+if [ `command -v dpkg` ]; then
+    sudo dpkg -l > $LOG_DIR/packages.txt
+elif [ `command -v rpm` ]; then
+    sudo rpm -qa > $LOG_DIR/packages.txt
+fi
+
+if [ `command -v docker` ]; then
+    sudo docker ps -a > $LOG_DIR/docker_ps_-a.txt
+    sudo docker images > $LOG_DIR/docker_images.txt
+fi
